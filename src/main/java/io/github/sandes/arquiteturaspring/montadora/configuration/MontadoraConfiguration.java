af69fbaec185b09aae0a7f4581df93c3a9aa60fd
Mateus Sandes <mateussandes@hotmail.com.br>
2024-12-30 20:07:30 -0300

    Adicionado outros tipos de motores e anotado o motorTurbo como primary

--- src/main/java/io/github/sandes/arquiteturaspring/montadora/configuration/MontadoraConfiguration.java
+++ src/main/java/io/github/sandes/arquiteturaspring/montadora/configuration/MontadoraConfiguration.java
@@ -4,22 +4,10 @@ import io.github.sandes.arquiteturaspring.montadora.Motor;
 import io.github.sandes.arquiteturaspring.montadora.TipoMotor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class MontadoraConfiguration {
-
-    @Bean(name = "motorAspirado")
-    public Motor motorAspirado(){
-        var motor = new Motor();
-
-        motor.setCavalos(120);
-        motor.setCilindors(4);
-        motor.setModelo("XPTO-0");
-        motor.setLitragem(2.0);
-        motor.setTipo(TipoMotor.ASPIRADO);
-
-        return motor;
-    }
 
     @Bean(name = "motorEletrico")
     public Motor motorEletrico(){
@@ -34,6 +22,20 @@ public class MontadoraConfiguration {
         return motor;
     }
 
+    @Bean(name = "motorAspirado")
+    public Motor motorAspirado(){
+        var motor = new Motor();
+
+        motor.setCavalos(120);
+        motor.setCilindors(4);
+        motor.setModelo("XPTO-0");
+        motor.setLitragem(2.0);
+        motor.setTipo(TipoMotor.ASPIRADO);
+
+        return motor;
+    }
+
+    @Primary //DEfinindo esse bean como o bean primario
     @Bean(name = "motorTurbo")
     public Motor motorTurbo(){
         var motor = new Motor();
