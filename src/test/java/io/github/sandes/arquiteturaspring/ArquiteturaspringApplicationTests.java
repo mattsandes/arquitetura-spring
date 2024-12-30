0a444686c1cc4c2b14d96e7783df69de54a74d7e
Mateus Sandes <mateussandes@hotmail.com.br>
2024-12-30 18:07:47 -0300

    Criado o projeto sobre arquitetura spring na prática

---
+++ src/test/java/io/github/sandes/arquiteturaspring/ArquiteturaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sandes.arquiteturaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
